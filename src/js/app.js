65c205f3e61f52c3de2cfab4e88adcab09d70fdb
KeshavPratap-K <keshavpratap@duck.com>
2026-09-18 12:10:51 +0530

    Update to rebble-clay and add gitignore

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-var Clay = require('pebble-clay');
+var Clay = require('@rebble/clay');
 var messageKeys = require('message_keys');
 var settings = require('./config');
 var customClay = require('./custom-clay');
