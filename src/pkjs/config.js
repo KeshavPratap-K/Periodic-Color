52d80c9b2496e807dfa1d156a425b7ac57ca849f
KeshavPratap-K <keshavpratap@duck.com>
2026-09-18 15:14:39 +0530

    update watchapp settings page

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -32,58 +32,44 @@ var DEFAULT_TEXT = DEFAULT_CARD.map(function(color) {
 
 var BW_FIVE = [['000000', '555555', 'aaaaaa', 'dddddd', 'ffffff']];
 
+function colorItem(id, key, label, value, colorLayout) {
+  return {
+    type: 'color',
+    id: id,
+    messageKey: key,
+    label: label,
+    defaultValue: value,
+    sunlight: false,
+    allowGray: true,
+    layout: colorLayout
+  };
+}
+
 function buildConfig(isColor) {
   var pickerLayout = isColor ? 'COLOR' : BW_FIVE;
-
-  // Build element options for select dropdown
-  var elementOptions = ELEMENTS.map(function(name, index) {
-    return { label: name, value: index };
-  });
-
-  return [
+  var page = [
     { type: 'heading', defaultValue: 'Periodic Color', size: 2 },
     { type: 'section', items: [
-      {
-        type: 'color',
-        id: 'background-color',
-        messageKey: 'BACKGROUND_COLOR',
-        label: 'Background Color',
-        defaultValue: '000000',
-        sunlight: false,
-        allowGray: true,
-        layout: pickerLayout
-      }
+      colorItem('background-color', 'BACKGROUND_COLOR', 'Background', '000000', pickerLayout)
     ] },
-    { type: 'heading', defaultValue: 'Element Color Customizer', size: 4 },
-    { type: 'section', items: [
-      {
-        type: 'select',
-        id: 'selected-element',
-        label: 'Select Element',
-        defaultValue: 0,
-        options: elementOptions
-      },
-      {
-        type: 'color',
-        id: 'card-color',
-        label: 'Card Color',
-        defaultValue: DEFAULT_CARD[0],
-        sunlight: false,
-        allowGray: true,
-        layout: pickerLayout
-      },
-      {
-        type: 'color',
-        id: 'text-color',
-        label: 'Text Color',
-        defaultValue: DEFAULT_TEXT[0],
-        sunlight: false,
-        allowGray: true,
-        layout: pickerLayout
-      }
-    ] },
-    { type: 'submit', defaultValue: 'Save Settings' }
+    { type: 'heading', defaultValue: 'Element colours', size: 4 },
+    { type: 'text', defaultValue: 'Choose a card and text colour for each element.' }
   ];
+
+  ELEMENTS.forEach(function(name, index) {
+    page.push({ type: 'section', items: [
+      { type: 'heading', defaultValue: name, size: 5 },
+      colorItem('card-' + index, 'CARD_COLOR[' + index + ']',
+                'Card colour', DEFAULT_CARD[index], pickerLayout),
+      colorItem('text-' + index, 'TEXT_COLOR[' + index + ']',
+                'Text colour', DEFAULT_TEXT[index], pickerLayout)
+    ] });
+  });
+
+  page.push({ type: 'button', id: 'reset-defaults',
+              defaultValue: 'Reset to original preset colours' });
+  page.push({ type: 'submit', defaultValue: 'Save settings' });
+  return page;
 }
 
 module.exports = {
--- wscript
+++ wscript
@@ -11,9 +11,12 @@ def build(ctx):
     ctx.load('pebble_sdk')
 
     binaries = []
+    original_env = ctx.env
+
     for platform in ctx.env.TARGET_PLATFORMS:
         ctx.env = ctx.all_envs[platform]
         ctx.set_group(ctx.env.PLATFORM_NAME)
+
         app_elf = '{}/pebble-app.elf'.format(ctx.env.BUILD_DIR)
         ctx.pbl_build(
             source=ctx.path.ant_glob('src/c/**/*.c'),
@@ -22,5 +25,10 @@ def build(ctx):
         )
         binaries.append({'platform': platform, 'app_elf': app_elf})
 
+    ctx.env = original_env
     ctx.set_group('bundle')
-    ctx.pbl_bundle(binaries=binaries)
+    ctx.pbl_bundle(
+        binaries=binaries,
+        js=ctx.path.ant_glob(['src/pkjs/**/*.js']),
+        js_entry_file='src/pkjs/index.js'
+    )
